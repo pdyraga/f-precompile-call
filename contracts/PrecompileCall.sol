2956097d96545bc0d834bbb83bca8db0bc470505
Piotr Dyraga <piotr.dyraga@akena.engineering>
2019-08-13 14:13:27 +0200

    Updated F precompile input data according to endianness changes

--- contracts/PrecompileCall.sol
+++ contracts/PrecompileCall.sol
@@ -18,7 +18,7 @@ contract PrecompileCall {
         return precompiledExpmod(5, 3, 13);
     }
 
-    function F(uint32 rounds, bytes32[2] memory h, bytes32[4] memory m, uint64[2] memory t, bool f) public view returns (bytes32[2] memory) {
+    function F(uint32 rounds, bytes32[2] memory h, bytes32[4] memory m, bytes8[2] memory t, bool f) public view returns (bytes32[2] memory) {
       bytes32[2] memory output;
 
       bytes memory args = abi.encodePacked(rounds, h[0], h[1], m[0], m[1], m[2], m[3], t[0], t[1], f);
@@ -34,37 +34,37 @@ contract PrecompileCall {
 
     // INPUT:
     // 0000000c
-    // b02406865b80cde9fec1e321559891be456158a6d71cf0eac1eb845c70d4a3d8
-    // 948e82cc975dcbd6984780bab7e236382fb585df5bc4b043cb3e866544413d92
-    // 87868584838281808f8e8d8c8b8a898897969594939291909f9e9d9c9b9a9998
-    // a7a6a5a4a3a2a1a0afaeadacabaaa9a8b7b6b5b4b3b2b1b0bfbebdbcbbbab9b8
-    // c7c6c5c4c3c2c1c0cfcecdcccbcac9c8d7d6d5d4d3d2d1d0dfdedddcdbdad9d8
-    // e7e6e5e4e3e2e1e0efeeedecebeae9e8f7f6f5f4f3f2f1f000fefdfcfbfaf9f8
-    // 000000000000017f
+    // 48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5
+    // d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b
+    // 6162630000000000000000000000000000000000000000000000000000000000
+    // 0000000000000000000000000000000000000000000000000000000000000000
+    // 0000000000000000000000000000000000000000000000000000000000000000
+    // 0000000000000000000000000000000000000000000000000000000000000000
+    // 0300000000000000
     // 0000000000000000
     // 01
     //
     // EXPECTED:
-    // ccfc282ed60927145b46f8d0fa97afd07010c51d20821e9748923ea42a37a0fa
-    // 0609a13be7c1e14b6e10a4b63d85d1d56d3d370d80f97b0a61a4f22ed6462dee
+    // ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d1
+    // 7d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923
     function callF() public view returns (bytes32[2] memory) {
       uint32 rounds = 12;
 
       bytes32[2] memory h;
-      h[0] = hex"b02406865b80cde9fec1e321559891be456158a6d71cf0eac1eb845c70d4a3d8"; 
-      h[1] = hex"948e82cc975dcbd6984780bab7e236382fb585df5bc4b043cb3e866544413d92";
+      h[0] = hex"48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5"; 
+      h[1] = hex"d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b";
   
       bytes32[4] memory m;
-      m[0] = hex"87868584838281808f8e8d8c8b8a898897969594939291909f9e9d9c9b9a9998";
-      m[1] = hex"a7a6a5a4a3a2a1a0afaeadacabaaa9a8b7b6b5b4b3b2b1b0bfbebdbcbbbab9b8";
-      m[2] = hex"c7c6c5c4c3c2c1c0cfcecdcccbcac9c8d7d6d5d4d3d2d1d0dfdedddcdbdad9d8";
-      m[3] = hex"e7e6e5e4e3e2e1e0efeeedecebeae9e8f7f6f5f4f3f2f1f000fefdfcfbfaf9f8";
+      m[0] = hex"6162630000000000000000000000000000000000000000000000000000000000";
+      m[1] = hex"0000000000000000000000000000000000000000000000000000000000000000";
+      m[2] = hex"0000000000000000000000000000000000000000000000000000000000000000";
+      m[3] = hex"0000000000000000000000000000000000000000000000000000000000000000";
  
-      uint64[2] memory t;
-      t[0] = 383; // 000000000000017f
-      t[1] = 0;	// 0000000000000000
+      bytes8[2] memory t;
+      t[0] = hex"03000000";
+      t[1] = hex"00000000";
 
-      bool f = true; // 01
+      bool f = true;
 
       return F(rounds, h, m, t, f);
     }
